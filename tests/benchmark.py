49b6d8fe98d64110eb66054019db84ecf83bffa7
Aryeh Leib Taurog <git@aryehleib.com>
2014-07-03 15:10:28 +0300

    Add standard executemany to benchmark

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -1,3 +1,4 @@
+import time
 from pgcopy import CopyManager
 from . import db
 
@@ -26,11 +27,29 @@ class Benchmark(db.TemporaryTable):
         mgr = self.manager(self.conn, self.table, self.cols)
         self.do_copy(mgr)
         self.check_count()
+        self.print_results(mgr.times.iteritems())
+
+    def print_results(self, results):
         print "-" * 70
         print "%s execution times:" % self.__class__.__name__
-        for name, time in mgr.times.iteritems():
-            print "%30s: %.2fs" % (name, time)
+        for name, elapsed_time in results:
+            print "%30s: %.2fs" % (name, elapsed_time)
+
 
 class ThreadingBenchmark(Benchmark):
     method = 'threading_copy'
     manager = CopyManager
+
+class ExecuteManyBenchmark(Benchmark):
+    def benchmark(self):
+        cols = ','.join(self.cols)
+        paramholders = ','.join(['%s'] * len(self.cols))
+        sql = "INSERT INTO %s (%s) VALUES (%s)" \
+                % (self.table, cols, paramholders)
+        cursor = self.conn.cursor()
+        start = time.time()
+        cursor.executemany(sql, self.data)
+        elapsed = time.time() - start
+        self.check_count()
+        self.print_results([('executemany', elapsed)])
+
