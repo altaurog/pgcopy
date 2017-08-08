ccce25b73bab61d8f73c12df63469b25eb4fedd0
Aryeh Leib Taurog <git@aryehleib.com>
2017-08-08 20:25:46 +0300

    test: add test for in-memory storage
    Verifies method documented in README (PR #9)

---
+++ tests/test_stringio.py
@@ -0,0 +1,13 @@
+from io import BytesIO
+from pgcopy import CopyManager, util
+from .test_datatypes import TypeMixin
+
+class Test(TypeMixin):
+    datatypes = ['integer', 'bool', 'varchar(12)']
+
+    def test_copy(self):
+        bincopy = CopyManager(self.conn, self.schema_table, self.cols)
+        bincopy.copy(self.data, BytesIO)
+        select_list = ','.join(self.cols)
+        self.cur.execute("SELECT %s from %s" % (select_list, self.table))
+        self.checkResults()
