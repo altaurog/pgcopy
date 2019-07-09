a4f2854f27ef3ed68b83ba87af02c42d6bb3b24d
Aryeh Leib Taurog <git@aryehleib.com>
2019-07-09 22:45:25 +0300

    test: add test for null value error

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -8,3 +8,8 @@ class TestErrors(db.TemporaryTable):
         col = self.cols[0] + '_does_not_exist'
         with pytest.raises(ValueError):
             CopyManager(conn, schema_table, [col])
+
+    def test_notnull(self, conn, schema_table):
+        bincopy = CopyManager(conn, schema_table, self.cols)
+        with pytest.raises(ValueError):
+            bincopy.copy([[None]])
