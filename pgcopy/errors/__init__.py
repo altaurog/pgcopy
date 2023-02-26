bf31a9e1fadb01c3e371c4808150255b321cd9c7
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2023-02-26 15:46:13 -0500

    [pgcopy/errors/__init__.py] `black`ening showed issue so manually moved `import` below module docstring

--- pgcopy/errors/__init__.py
+++ pgcopy/errors/__init__.py
@@ -1,8 +1,9 @@
-import sys
 """
 Decorating and re-raising an exception is a pain in Python 2
 Python 3 "fixes" this pain by introducing new syntax!
 """
+
+import sys
 
 if sys.version_info[0] == 2:
     from .py2 import raise_from
